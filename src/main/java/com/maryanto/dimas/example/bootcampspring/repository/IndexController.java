da787c8d48f34bdf0c1b5aa4db3aead4ae3e253c
aryaprimaaaa <aryaprima991@gmail.com>
2022-01-19 15:22:45 +0700

    punya arya

---
+++ src/main/java/com/maryanto/dimas/example/bootcampspring/repository/IndexController.java
@@ -0,0 +1,40 @@
+package com.maryanto.dimas.example.bootcampspring.repository;
+
+import com.maryanto.dimas.example.bootcampspring.entity.Department;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/web")
+public class IndexController {
+
+    @Autowired
+    private DepartmentRepository repo;
+
+    @GetMapping("/index")
+    public String showIndex(Model model) {
+        List<Department> list = this.repo.list();
+        model.addAttribute("data", new Department());
+        model.addAttribute("listDepartment", list);
+        return "index";
+    }
+
+    @PostMapping("/test")
+    public String processRequest(
+            @RequestParam(name = "nama") String nama,
+            @RequestParam("description") String description) {
+        System.out.println("nama: " + nama + ", description: " + description);
+        return "redirect:/web/index";
+    }
+
+    @PostMapping("/test2")
+    public String processRequest2(
+            @ModelAttribute Department dept) {
+        this.repo.insert(dept);
+        return "redirect:/web/index";
+    }
+}
